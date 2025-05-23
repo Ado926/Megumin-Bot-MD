2c71d2ee042736930aefd0e073442e29bd172b4c
Ado <adonaylester85@gmail.com>
2025-05-23 15:34:44 -0600

    Update handler.js

--- megumin/handler.js
+++ megumin/handler.js
@@ -503,13 +503,13 @@ if (opts['autoread']) await this.readMessages([m.key])
 
 global.dfail = (type, m, conn) => {
 const msg = {
-rowner: '「👑」 *Esta función solo puede ser usada por mi creador*\n\n> David-Chian.', 
+rowner: '「👑」 *Esta función solo puede ser usada por mi creador*\n\n> Wirk ☔', 
 owner: '「👑」 *Esta función solo puede ser usada por mi desarrollador.', 
 premium: '「🍧」 *Esta función solo es para usuarios Premium.',  
 private: '「🍭」 *Esta función solo puede ser usada en chat privado.*', 
 admin: '「👑」 *Este comando solo puede ser usado por admins.*', 
 botAdmin: '「🚩」 *Para usar esta función debo ser admin.*', 
-unreg: '「🍟」 *¡Hey! no estas registrado, registrese para usar esta función*\n\n*/reg nombre.edad*\n\n*_❕ Ejemplo_* : */reg David.23*',
+unreg: '「☔」 *Hey no estas registrado, registrese para usar esta función*\n\n*/reg nombre.edad*\n\n*_❕ Ejemplo_* : */reg Ado.73*',
 restrict: '「💫」 *Esta característica esta desactivada.*'
 }[type];
 if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'))}
