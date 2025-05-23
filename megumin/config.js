32a95b24515c9906eabfb62ae010606b3d22a769
Ado <adonaylester85@gmail.com>
2025-05-23 15:31:41 -0600

    Update config.js

--- megumin/config.js
+++ megumin/config.js
@@ -15,49 +15,63 @@ global.confirmCode = ''
 
 //• ↳𝑺𝑶𝑳𝑶 𝑫𝑬𝑺𝑨𝑹𝑹𝑶𝑳𝑳𝑨𝑫𝑶𝑹𝑬𝑺 𝑨𝑷𝑹𝑶𝑩𝑨𝑫𝑶𝑺
 global.owner = [
-  ['5351524614', '🔥 𝗖𝗿𝗲𝗮𝗱𝗼𝗿 (◣_◢)凸', true],
-  ['5492916450307'],
-  ['5218715746374'],
-  ['5218711426787']
+  ['50493732693', '🜲 Propietario 🜲', true],
+  ['5216671548329']
 ];
 
-global.suittag = ['5351524614']
+global.suittag = ['50493732693']
 global.prems = []
 
 global.libreria = 'Baileys'
 global.baileys = 'fizzxydev/baileys-pro'
 global.vs = '^2.0.5'
 global.languaje = 'Español'
-global.nameqr = 'Megumin Bot - MD'
+global.nameqr = 'MichiAi-MD'
 global.sessions = 'MeguminSession'
 global.jadi = 'MeguminJadiBot'
 global.meguminJadibts = true
 
 //• ↳ ◜𝑴𝑨𝑹𝑪𝑨𝑺 𝑫𝑬 𝑨𝑮𝑼𝑨◞ • 💌
-global.packsticker = `♾ ━━━━━━━━\n├ ɓσƭ:\n├ ρяοριєταяιο:\n├ ƒєϲнα ∂є ϲяєαϲιόи:\n├ нοяα:\n♾━━━━━━━━`
-global.packname = `🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)`
-global.author = `♾━━━━━━━━\n⇝͟͞ ۵̤͟𝚳̶̤𝚵̅𝔾̈͟𝖀𝐌̶̤𝚰̅𝚴̈͟♡̵̑ ͟͞   ᷨᜳ ͦ ̵ͭ⋆\n⇝ ۵-̱̅𝐃𝖆𝒗𝖏𝖉𐝃𐋃ᶜʱⁱᵃᶯ-͞ˍ\n⇝ ${moment.tz('America/Los_Angeles').format('DD/MM/YY')}\n⇝ ${moment.tz('America/Los_Angeles').format('HH:mm:ss')} \n♾━━━━━━━━\n\n\n\nѕτιϲκєя ϐγ: ৎ୭࠭͢𝑴𝒆̤𝒈𝒖̣֟፝֯𝒎̤𝒊̣𝒏🔥̤ʙⷪᴏ͓ᷫᴛⷭ𓆪͟͞ `;
-global.wm = 'ৎ୭࠭͢𝑴𝒆̤𝒈𝒖̣֟፝֯𝒎̤𝒊̣𝒏🔥̤ʙⷪᴏ͓ᷫᴛⷭ𓆪͟͞ ';
-global.titulowm = '͟͞ ৎ୭࠭͢💥͟𝑬̶𝒙͞𝒑͟𝒍𝒐̶𝒔̅𝒊͟𝒐́𝒏̶🔥̤𓆪 ͟͞';
-global.titulowm2 = `͟͞ ৎ୭࠭͢💥͟𝑬̶𝒙͞𝒑͟𝒍𝒐̶𝒔̅𝒊͟𝒐́𝒏̶🔥̤𓆪 ͟͞`
-global.igfg = 'ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆̟(◣_◢)凸'
-global.botname = '⏤͟͞ू⃪ ፝͜⁞M͢ᴇɢ፝֟ᴜᴍ⃨ɪɴ⃜✰⃔࿐'
-global.dev = '© ⍴᥆ᥕᥱrᥱძ ᑲᥡ ძᥲ᥎іძ ᥴһіᥲᥒ ❀'
-global.textbot = '⏤͟͞ू⃪ ፝͜⁞M͢ᴇɢ፝֟ᴜᴍ⃨ɪɴ⃜✰⃔࿐ : ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆̟(◣_◢)凸'
-global.gt = '͟͞ ৎ୭࠭͢💥͟𝑬̶𝒙͞𝒑͟𝒍𝒐̶𝒔̅𝒊͟𝒐́𝒏̶🔥̤𓆪 ͟͞';
-global.namechannel = '⏤͟͞ू⃪ ፝͜⁞M͢ᴇɢ፝֟ᴜᴍ⃨ɪɴ⃜✰⃔࿐/ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆̟(◣_◢)凸'
-
+global.packsticker = `☁️━━━━━━━━━━━━\n├ Bot:\n├ Creador:\n├ Fecha de creación:\n├ Hora actual:\n☁️━━━━━━━━━━━━`
+global.packname = '⪛✰ 𝗠𝗶𝗰𝗵𝗶-𝗔𝗶-𝗕𝗼𝘁 ✰⪜'
+global.botname = '☁️ 𓃠 𝗠𝗶𝗰𝗵𝗶・𝗔𝗜・𝗕𝗼𝘁 ✩'
+global.wm = '☁️ Michi_Ai-Bot by Wirk'
+global.author = `☁️━━━━━━━━━━━━\n➤ Creado por: Wirk\n➤ Fecha: ${moment.tz('America/Los_Angeles').format('DD/MM/YY')}\n➤ Hora: ${moment.tz('America/Los_Angeles').format('HH:mm:ss')}\n☁️━━━━━━━━━━━━\nSticker hecho por: Michi Ai Bot`
+global.dev = '© Powered By Wirk'
+global.textbot = '☁️ Michi Ai Bot • Creado por Wirk'
+global.titulowm = '☁️ Explosión de comandos • Michi Ai Bot'
+global.titulowm2 = '☁️ Michi Ai Bot en acción'
+global.igfg = 'Wirk Project (◣_◢)'
+global.gt = '☁️ Michi Ai Bot - Funcionalidades únicas'
+global.namechannel = '☁️ Michi Ai Bot / Wirk Project'
+global.etiqueta = 'Wirk'
 
 //• ↳ ◜𝑰𝑴𝑨́𝑮𝑬𝑵𝑬𝑺◞ • 🌇
-global.imagen1 = 'https://files.catbox.moe/6lhjit.jpg'
-global.imagen2 = 'https://files.catbox.moe/bm0q3s.jpg'
-global.imagen3 = 'https://files.catbox.moe/zrbazr.jpg'
-global.imagenadult = 'https://files.catbox.moe/3dbuvw.jpg'
-global.logo = 'https://files.catbox.moe/magmik.jpg'
+global.imagen1 = 'https://qu.ax/EbklL.jpg'
+global.imagen2 = 'https://qu.ax/EbklL.jpg'
+global.imagen3 = 'https://qu.ax/EbklL.jpg'
+global.imagenadult = 'https://qu.ax/EbklL.jpg'
+global.logo = 'https://qu.ax/EbklL.jpg'
 
 //• ↳ ◜𝑭𝑨𝑲𝑬 𝑬𝑺𝑻𝑰𝑳𝑶◞ • 🪩
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '⏤͟͞ू⃪ ፝͜⁞M͢ᴇɢ፝֟ᴜᴍ⃨ɪɴ⃜✰⃔࿐', orderTitle: 'Bang', thumbnailUrl: logo, sellerJid: '0@s.whatsapp.net'}}}
-
+global.estilo = {
+  key: {
+    fromMe: false,
+    participant: `0@s.whatsapp.net`,
+    ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {})
+  },
+  message: {
+    orderMessage: {
+      itemCount: -999999,
+      status: 1,
+      surface: 1,
+      message: '⏤͟͞ू⃪ ፝͜⁞Mɪᴄʜɪ Aɪ Bᴏᴛ✰⃔࿐',
+      orderTitle: 'Bang',
+      thumbnailUrl: logo,
+      sellerJid: '0@s.whatsapp.net'
+    }
+  }
+}
 //• ↳ ◜𝑳𝑰𝑵𝑲𝑺◞ • 🌿
 global.ofcgp = 'https://chat.whatsapp.com/F4QEFF2Hn4102NdbPJ2ZOi' //Grupo Oficial De Megumin
 global.gp1 = 'https://chat.whatsapp.com/DSz2abnPgfE4IzZjynQLu3' //Grupo de Kotori Bot
@@ -65,14 +79,14 @@ global.gp2 = 'https://chat.whatsapp.com/J9gyFJLbhVIJXaUZlpo8Xt'//Grupo de enlace
 global.comunidad1 = 'https://chat.whatsapp.com/DWQb1xZClyR98ogvwI3qae' //Comunidad Megumin
 global.channel = 'https://whatsapp.com/channel/0029VaqAtuIK0IBsHYXtvA3e' //Canal Oficial
 global.channel2 = 'https://whatsapp.com/channel/0029Vb7Ji66KbYMTYLU9km3p' //Canal de Legends
-global.yt = 'https://youtube.com/@davidchian4957' //Canal De Youtube
-global.md = 'https://github.com/David-Chian/Megumin-Bot-MD' //Github Oficial
-global.correo = 'noisebot@gmail.com'
+global.yt = 'https://youtube.com/@Moitwr' //Canal De Youtube
+global.md = 'https://github.com/Ado926/Mai2' //Github Oficial
+global.correo = 'minexdt@gmail.com'
 
 var ase = new Date(); var hour = ase.getHours(); switch(hour){ case 0: hour = 'Linda Mañana'; break; case 1: hour = 'Linda Mañana'; break; case 2: hour = 'Linda Mañana'; break; case 3: hour = 'Linda Mañana'; break; case 4: hour = 'linda mañana'; break; case 5: hour = 'Linda Mañana'; break; case 6: hour = 'Linda Mañana'; break; case 7: hour = 'Linda Mañana'; break; case 8: hour = 'Linda Mañana'; break; case 9: hour = 'Linda Mañana'; break; case 10: hour = 'Lindo Dia'; break; case 11: hour = 'Lindo Dia'; break; case 12: hour = 'Lindo Dia'; break; case 13: hour = 'Lindo Dia'; break; case 14: hour = 'Linda Tarde'; break; case 15: hour = 'Linda Tarde'; break; case 16: hour = 'Linda Tarde'; break; case 17: hour = 'Linda Tarde'; break; case 18: hour = 'Linda Noche'; break; case 19: hour = 'Linda Noche'; break; case 20: hour = 'Linda Noche'; break; case 21: hour = 'Linda Noche'; break; case 22: hour = 'Linda Noche'; break; case 23: hour = 'Linda Noche'; break;}
 global.saludo = '🍭' + hour;
 
-global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363358338732714@newsletter", serverMessageId: 100, newsletterName: namechannel, }, }, }
+global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363402846939411@newsletter", serverMessageId: 100, newsletterName: namechannel, }, }, }
 //• ↳ ◜𝑻𝑰𝑬𝑴𝑷𝑶◞ • 🕒
 global.d = new Date(new Date + 3600000);
 global.locale = 'es';
